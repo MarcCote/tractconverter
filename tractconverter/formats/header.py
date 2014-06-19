bccd598be5d4c5af63c4528a5c0dfb3bdf32f5cb
Marc-Alexandre Cote <marc.cote.19@gmail.com>
2014-06-19 13:20:22 -0400

    Merge PR #29

--- tractconverter/formats/header.py
+++ tractconverter/formats/header.py
@@ -4,7 +4,6 @@ Created on 2012-02-22
 @author: coteharn
 '''
 import nibabel as nib
-import numpy as np
 
 
 class Header:
@@ -35,6 +34,7 @@ def get_header_from_anat(anat_file, hdr={}):
 
     anat = nib.load(anat_file)
 
+    hdr[Header.NB_FIBERS] = 0
     hdr[Header.VOXEL_SIZES] = tuple(anat.get_header().get_zooms())[:3]
     hdr[Header.DIMENSIONS] = tuple(anat.get_header().get_data_shape())
     hdr[Header.VOXEL_TO_WORLD] = anat.get_header().get_best_affine()
