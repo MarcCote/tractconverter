022606c17bc759f5a7f7394a4835caa0e6d68ad2
Marc-Alexandre Cote <marc.cote.19@gmail.com>
2014-06-30 15:47:16 -0400

    Now one can access TCK,TRK,VTK and FIB directly from module tractconverter.

--- tractconverter/__init__.py
+++ tractconverter/__init__.py
@@ -4,3 +4,8 @@ from tractconverter.utils import is_supported
 from tractconverter.utils import detect_format
 from tractconverter.utils import FORMATS
 from tractconverter.utils import EXT_ANAT
+
+from tractconverter.formats.tck import TCK
+from tractconverter.formats.trk import TRK
+from tractconverter.formats.vtk import VTK
+from tractconverter.formats.fib import FIB
